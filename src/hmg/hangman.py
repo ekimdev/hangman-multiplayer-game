c0d619f0f018024b2e2f0621d93aa122cfee2e13
Rodrigo Acosta <rodrigoacosta444@gmail.com>
2021-12-17 03:38:19 -0300

    feat: add start_game method
    este metodo iniciaria las funciones del juego

--- src/hmg/hangman.py
+++ src/hmg/hangman.py
@@ -9,6 +9,8 @@ class Hangman():
         self.nombre_jugador = nombre_jugador
         self.palabra_secreta = get_word(words)
         self.letras_palabra_secreta = set(self.palabra_secreta)
+        self.letras_usadas = set()
+        self.tablero = ["_" for _ in range(len(self.palabra_secreta))]
         self.vidas = 5
 
     def letra_is_valid(self, letra):
@@ -19,6 +21,33 @@ class Hangman():
 
     def pedir_letra(self):
         return input('Ingresa una letra: ')
+
+    def comprobar_letras_usadas(self, letra_ingresada):
+        if letra_ingresada not in self.letras_usadas:
+            self.letras_usadas.add(letra_ingresada)
+        else:
+            print(f'Ya ingresaste la letra {letra_ingresada}')
+
+    def terminar_juego(self):
+        if self.vidas == 0:
+            print('Se acabo el juego, Perdiste!!')
+        else:
+            print('Ganaste!!')
+
+    def start_game(self):
+        while self.vidas > 0:
+            print(self.tablero)
+
+            letra_ingresada = self.pedir_letra()
+            if not self.letra_is_valid(letra_ingresada):
+                letra_ingresada = pedir_letra() + 'Valida'
+            self.comprobar_letras_usadas(letra_ingresada)
+            if letra_ingresada in self.letras_palabra_secreta:
+                self.letras_palabra_secreta.remove(letra_ingresada)
+            else:
+                self.vidas = self.vidas - 1
+
+        self.terminar_juego()
 
 def get_word(archivo):
     word = random.choice(words)
