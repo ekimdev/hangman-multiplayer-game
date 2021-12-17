a473c0b76e1b83d5bfdd9dcdfb4ff2579380eec1
Gabriel Acosta <acostadariogabriel@gmail.com>
2021-12-16 21:02:07 -0300

    feat: function prototypes for obtain word

---
+++ src/hmg/word_utils.py
@@ -0,0 +1,16 @@
+"""
+Módulo con funciones para obtener una palabra desde
+distintos medios
+"""
+
+
+def get_word_from_internet() -> str:
+    pass
+
+
+def get_word_from_file(filename: str) -> str:
+    pass
+
+
+def get_word_from_list() -> str:
+    pass
