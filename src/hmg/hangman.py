b5bfcb882c718c02008e77188253ae14c2281bc1
Rodrigo Acosta <rodrigoacosta444@gmail.com>
2021-12-17 00:51:27 -0300

    feat: add a pedir_letra and letra_is_valid method
    los metodos solicitan al usuario ingresar una letra y verifican si esta
    es valida o no
    tambien se agregaron los tests correspondientes

--- src/hmg/hangman.py
+++ src/hmg/hangman.py
@@ -1,14 +1,24 @@
 import random
 
 
-words = ["aback", "abaft", "abandoned", "abashed", "aberrant"]
+words = ["aback"]
 
 
 class Hangman():
-    def __init__(self, nombre_jugador):
+    def __init__(self, nombre_jugador='player_default'):
         self.nombre_jugador = nombre_jugador
+        self.palabra_secreta = get_word(words)
+        self.letras_palabra_secreta = set(self.palabra_secreta)
         self.vidas = 5
 
+    def letra_is_valid(self, letra):
+        letra_valid = True
+        if letra.isdigit() or not letra:
+            letra_valid = False
+        return letra_valid
+
+    def pedir_letra(self):
+        return input('Ingresa una letra: ')
 
 def get_word(archivo):
     word = random.choice(words)
