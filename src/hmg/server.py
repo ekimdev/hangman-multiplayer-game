126427b570531b1e38f504982cbeb8a9e1b87f4e
Rodrigo Acosta <rodrigoacosta444@gmail.com>
2022-02-18 00:23:33 -0300

    feat: add a server file

--- src/hmg/server.py
+++ src/hmg/server.py
@@ -1,39 +1,69 @@
 import socket
 import threading
-
-host = ''
-port = 8001
-BUFFER_SIZE = 1024
-HOSTNAME = socket.gethostname()
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, port))
-server.listen(1)
+import argparse
+import pickle
 
 
-def handle_connection(sock):
-    """
-        esta funcion maneja las conexiones de los clientes al servidor
-    """
-    msg = server.recv(BUFFER_SIZE)
-    if not msg:
-        print('Cliente desconectado')
-    else:
-        print(f'El cliente a enviado {msg.decode()}')
+class Server:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._players = {}
+
+    def serve_forever(self):
+        self._socket.bind((self.host, self.port))
+        self._socket.listen(2)
+        print(f'[*] Server listening on {self.host}:{self.port}')
+        self._listen_connection()
+
+    def _listen_connection(self):
+        is_listening = True
+        while is_listening:
+            if len(self._players) == 2:
+                player1, player2 = self._players.values()
+                payload = {'turn': False, 'win': False, 'msg': ''}
+                player1.send(pickle.dumps(payload))
+                payload = {'turn': True, 'win': False, 'msg': ''}
+                player2.send(pickle.dumps(payload))
+
+            conn, addr = self._socket.accept()
+            data = pickle.loads(conn.recv(1024))
+            username = data['user']
+            print(f'[*] {username}[{addr}] connected')
+            self._players[username] = conn
+
+            client_thread = threading.Thread(
+                target=self._handle_client, args=(conn, addr, username))
+            client_thread.start()
+
+    def _handle_client(self, client, addr, username):
+        client.send(pickle.dumps({'msg': 'bandera'}))
+
+        is_online = True
+        while is_online:
+            print('[*] Waiting for client message...')
+            msg_client = client.recv(1024)
+            if not msg_client:
+                print(f'[*] Client {username} disconnected')
+                del self._players[username]
+                is_online = False
+
+            data = pickle.loads(msg_client)
+            print(f'[*] Sending everyone {data}')
+
+            for user, sock in self._players.items():
+                if user != username:
+                    data['turn'] = True
+                    sock.send(pickle.dumps(data))
+                else:
+                    data['turn'] = False
+                    sock.send(pickle.dumps(data))
 
 
-def main():
-    while True:
-        print('Esperando conexion...')
-        sock_client, addr = server.accept()
-        print(f'Cliente {addr} conectado')
+def cli_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-H', '--host', default=socket.gethostname())
+    parser.add_argument('-p', '--port', default=8889)
 
-        t1 = threading.Thread(target=handle_connection, args=(sock_client,))
-        t1.start()
-
-
-try:
-    main()
-except KeyboardInterrupt:
-    server.shutdown(socket.SHUT_WR)
-    server.close()
+    return parser
