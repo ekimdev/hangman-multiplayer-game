b89b2b565b3f03252c89338a7f3819a4c8612595
Rodrigo Acosta <rodrigoacosta444@gmail.com>
2022-03-10 20:50:39 -0300

    refact: remove start_game function
    se elimino la funcion start_game y funciones que ya no seran utilizadas
    se eliminaron imports que no se utilizaran
    se agregaron nuevas funciones, for more info #20
    
    closes #20

--- src/hmg/hangman.py
+++ src/hmg/hangman.py
@@ -1,17 +1,10 @@
-from hmg.display import clean_screen, draw_header
-
-
 class Hangman:
-    def __init__(
-        self, palabra_secreta, char_tablero="-", nombre_jugador="player_default"
-    ):
-        self.nombre_jugador = nombre_jugador
-        self.palabra_secreta = palabra_secreta
+    def __init__(self, palabra_secreta, char_tablero="-"):
+        self.palabra_secreta = list(palabra_secreta)
         self.letras_palabra_secreta = set(self.palabra_secreta)
         self.letras_usadas = set()
         self.char_tablero = char_tablero
         self.tablero = [self.char_tablero for _ in range(len(self.palabra_secreta))]
-        self.vidas = 5
 
     def letra_is_valid(self, letra):
         letra_valid = True
@@ -28,33 +21,18 @@ class Hangman:
         else:
             print(f"Ya ingresaste la letra {letra_ingresada}")
 
-    def terminar_juego(self):
-        if self.vidas == 0:
-            print("Se acabo el juego, Perdiste!!")
-        else:
-            print("Ganaste!!")
+    def letra_esta_en_la_palabra(self, letra_ingresada):
+        if letra_ingresada in self.letras_palabra_secreta:
+            self.letras_palabra_secreta.remove(letra_ingresada)
+            return True
+        return False
 
-    def start_game(self):
-        while len(self.letras_palabra_secreta) > 0 and self.vidas > 0:
-            clean_screen()
-            draw_header(self.tablero, self.letras_usadas, self.vidas)
+    def actualizar_tablero(self, letra_ingresada):
+        for _ in range(self.palabra_secreta.count(letra_ingresada)):
+            index = self.palabra_secreta.index(letra_ingresada)
+            self.palabra_secreta.pop(index)
+            self.palabra_secreta.insert(index, self.char_tablero)
+            self.tablero.pop(index)
+            self.tablero.inser(index, letra_ingresada)
 
-            letra_ingresada = self.pedir_letra().upper()
-            if not self.letra_is_valid(letra_ingresada):
-                print("Por favor ingresa una letra valida\n")
-                continue
-
-            for _ in range(self.palabra_secreta.count(letra_ingresada)):
-                index = self.palabra_secreta.index(letra_ingresada)
-                self.palabra_secreta.pop(index)
-                self.palabra_secreta.insert(index, self.char_tablero)
-                self.tablero.pop(index)
-                self.tablero.insert(index, letra_ingresada)
-
-            self.comprobar_letras_usadas(letra_ingresada)
-            if letra_ingresada in self.letras_palabra_secreta:
-                self.letras_palabra_secreta.remove(letra_ingresada)
-            else:
-                self.vidas = self.vidas - 1
-
-        self.terminar_juego()
+        self.comprobar_letras_usadas(letra_ingresada)
